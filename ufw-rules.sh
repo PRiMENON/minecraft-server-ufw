fcc6de3240fb49eb08735a951325373c62b834f8
PRiMENON <kuroweb@gmail.com>
2025-05-08 22:12:39 +0900

    update

--- ufw-rules.sh
+++ ufw-rules.sh
@@ -13,35 +13,19 @@ sudo ufw default deny
 # deny section
 ######################################################
 
-sudo ufw deny from 138.199.43.0/24
-sudo ufw deny from 5.231.70.0/24
-sudo ufw deny from 23.96.0.0/13
-sudo ufw deny from 35.195.0.0/16
-sudo ufw deny from 45.135.194.0/24
-sudo ufw deny from 45.148.10.0/24
-sudo ufw deny from 45.83.220.0/24
-sudo ufw deny from 45.95.147.0/24
-sudo ufw deny from 46.101.0.0/16
-sudo ufw deny from 65.49.0.0/18
-sudo ufw deny from 69.48.192.0/19
-sudo ufw deny from 76.112.0.0/12
-sudo ufw deny from 82.66.192.0/18
-sudo ufw deny from 91.92.40.0/22
-sudo ufw deny from 94.131.100.0/24
-sudo ufw deny from 97.112.0.0/12
-sudo ufw deny from 97.80.0.0/12
 sudo ufw deny from 103.154.143.0/24
 sudo ufw deny from 103.75.8.0/22
 sudo ufw deny from 111.105.32.0/19
 sudo ufw deny from 130.61.0.0/16
+sudo ufw deny from 138.199.43.0/24
 sudo ufw deny from 141.98.255.0/24
-sudo ufw deny from 146.70.189.0/24
-sudo ufw deny from 146.70.225.0/24
 sudo ufw deny from 146.70.117.0/24
-sudo ufw deny from 146.70.144.0/24
-sudo ufw deny from 146.70.197.0/24
-sudo ufw deny from 146.70.128.0/24
 sudo ufw deny from 146.70.119.0/24
+sudo ufw deny from 146.70.128.0/24
+sudo ufw deny from 146.70.144.0/24
+sudo ufw deny from 146.70.189.0/24
+sudo ufw deny from 146.70.197.0/24
+sudo ufw deny from 146.70.225.0/24
 sudo ufw deny from 149.102.246.0/24
 sudo ufw deny from 151.115.0.0/17
 sudo ufw deny from 154.213.192.0/24
@@ -57,6 +41,22 @@ sudo ufw deny from 198.54.132.0/24
 sudo ufw deny from 199.58.80.0/22
 sudo ufw deny from 209.38.96.0/19
 sudo ufw deny from 212.83.128.0/17
+sudo ufw deny from 23.96.0.0/13
+sudo ufw deny from 35.195.0.0/16
+sudo ufw deny from 45.135.194.0/24
+sudo ufw deny from 45.148.10.0/24
+sudo ufw deny from 45.83.220.0/24
+sudo ufw deny from 45.95.147.0/24
+sudo ufw deny from 46.101.0.0/16
+sudo ufw deny from 5.231.70.0/24
+sudo ufw deny from 65.49.0.0/18
+sudo ufw deny from 69.48.192.0/19
+sudo ufw deny from 76.112.0.0/12
+sudo ufw deny from 82.66.192.0/18
+sudo ufw deny from 91.92.40.0/22
+sudo ufw deny from 94.131.100.0/24
+sudo ufw deny from 97.112.0.0/12
+sudo ufw deny from 97.80.0.0/12
 
 ######################################################
 # allow section
